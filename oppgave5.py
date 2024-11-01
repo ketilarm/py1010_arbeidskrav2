b806d3697c243391cad8b9296a4527f8bfc5c56e
Ketil <102487845+ketilarm@users.noreply.github.com>
2024-11-01 23:08:28 +0100

    Create oppgave5.py

---
+++ oppgave5.py
@@ -0,0 +1,5 @@
+"""Oppg 5) Lag et program med en funksjon som tar a og b som inn-argumenter og som så 
+regner ut arealet og «ytre» omkrets til en figur satt sammen av en rettvinklet trekant og en 
+halvsirkel, se figuren under. Med «ytre» omkrets menes samlet lengde av de sorte strekene. 
+Funksjonen skal returnere arealet og «ytre» omkrets, som så skrives til skjerm med passende 
+"""
