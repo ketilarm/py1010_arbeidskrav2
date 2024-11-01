951ee7a1d6e1d5c633dd1786688c154a70717463
Ketil <102487845+ketilarm@users.noreply.github.com>
2024-11-01 23:02:52 +0100

    Create oppgave1.py

---
+++ oppgave1.py
@@ -0,0 +1,4 @@
+"""Oppg 1) Du skal her lage et program som skal starter med 
+alder = int(input('Hvilket år er du født? ') )
+Programmet skal så regne ut hvor gammel personen blir nå i løpet av år 2024 og skrive 
+svaret til skjerm med passende tekst."""
