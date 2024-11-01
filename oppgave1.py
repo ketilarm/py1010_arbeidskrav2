fc3391622089224a49a528efcc9adbb6ce1994e3
Ketil <102487845+ketilarm@users.noreply.github.com>
2024-11-01 23:15:08 +0100

    .

--- oppgave1.py
+++ oppgave1.py
@@ -2,3 +2,9 @@
 alder = int(input('Hvilket år er du født? ') )
 Programmet skal så regne ut hvor gammel personen blir nå i løpet av år 2024 og skrive 
 svaret til skjerm med passende tekst."""
+
+
+alder = int(input('Hvilket år er du født? '))
+årstall=2024
+
+print(f"Du blir {årstall-alder} år i løpet av 2024")
