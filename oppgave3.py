a4f68efaddbc14c957422cd4c52e62c7dd6c7831
Ketil <102487845+ketilarm@users.noreply.github.com>
2024-11-01 23:04:26 +0100

    Create oppgave3.py

---
+++ oppgave3.py
@@ -0,0 +1,7 @@
+"""Oppg 3) Lag et program med en funksjon som regner om fra grader til radianer. 
+Programmet skal starte med: 
+import numpy as np 
+v_grad = float(input('Skriv inn gradtallet:' ))
+Radiantallet til vinkelen regnes så ut ved følgende formel: v_rad = v_grad*np.pi/180
+Resultatet v_rad skrives til skjerm med passende tekst og verdi. 
+Merk: np.pi er en ferdiglaget funksjon som gir verdien 3.1415...."""
