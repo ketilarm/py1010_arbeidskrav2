5c4ffeaf176dbaed1f31d5dd251ccb5adedc4f7b
Ketil <102487845+ketilarm@users.noreply.github.com>
2024-11-01 23:07:54 +0100

    Create oppgave4.py

---
+++ oppgave4.py
@@ -0,0 +1,21 @@
+"""Oppg 4) 
+a) Opprett en dictionary som gitt under. Dictionaryen har ulike land som nøkkel (Keys) 
+og gir info om hovedstaden i landet og antall innbyggere i mill. i hovedstaden. 
+
+
+
+b)Lag et program som ber brukeren skrive inn et land (eksempelvis England). 
+Programmet skal på bakgrunn av dette skrive ut følgende setning:
+London er hovedstaden i England og det er 8.982 mill. innbyggere i London
+c) Lag et program som ber brukeren skrive inn info om et nytt land (altså et land som 
+ikke allerede finnes i dictionaryen data). Videre skal brukeren oppgi hovedstad og 
+antall innbyggere for det «nye» landet. Programmet skal så utvide/oppdatere
+dictionaryen med den nye informasjonen. Dictionaryen data skrives så til skjerm."""
+
+data ={
+  "Norge":["Oslo",0.634],
+  "England":["London",8.982],
+  "Frankrike":["Paris",2.161],
+  "Italia":["Roma",2.873],
+}
+
