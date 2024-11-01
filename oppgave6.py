0188669a5e94fc97386bd0976128857e76f2b1ca
Ketil <102487845+ketilarm@users.noreply.github.com>
2024-11-01 23:09:08 +0100

    Create oppgave6.py

---
+++ oppgave6.py
@@ -0,0 +1,5 @@
+"""Oppg 6) Skriv en kode som plotter funksjonen 𝑓(𝑥) = −𝑥2 − 5, for x på intervallet [-10,10].
+Hint: np.linspace(-10, 10, 200) gir en array med 200 punkter jevnt fordelt på intervallet 
+[-10,10].
+Selv om Arbeidskrav 2 er en individuell innlevering kan du selvsagt diskutere med kollegaer 
+og søke hjelp fra våre studentassistenter underveis."""
