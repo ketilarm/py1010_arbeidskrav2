f112d44c725e3160b99aef8692af959d38471878
Ketil <102487845+ketilarm@users.noreply.github.com>
2024-11-01 23:03:42 +0100

    Create oppgave2.py

---
+++ oppgave2.py
@@ -0,0 +1,9 @@
+"""Oppg 2) Det skal arrangeres en klassefest og man antar at hver elev spiser 1/4 pizza. Lag et 
+program som tar inn antall elever fra konsollen ved 
+antall_elever = int(input('Skriv inn antall elever:' ))
+Programmet skal så regne ut hvor mange pizzaer som skal handles inn til festen og skrive 
+svaret til skjerm. Merk, man kan ikke kjøpe 4 og en kvart pizza på butikken (man må da kjøpe 
+5). 
+Hint1: Gir programmet ditt et fornuftig svar hvis det f.eks er 21 elever i klassen? 
+Hint2: Det er ikke vanlig å si/skrive: ‘Det må handles inn 6.0 pizzaer til festen’. Hvordan kan 
+sikre at antall pizzaer skrives ut som et heltall (ikke desimaltall)? """
