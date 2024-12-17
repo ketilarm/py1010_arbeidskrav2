d735b52f4abb5d04e8958bf7b2014a5680ee1b1e
Ketil <102487845+ketilarm@users.noreply.github.com>
2024-12-17 13:17:18 +0100

    Update oppgave1.py

--- oppgave1.py
+++ oppgave1.py
@@ -1,10 +1,12 @@
-"""Oppg 1) Du skal her lage et program som skal starter med 
+"""Oppg 1) Du skal her lage et program som skal starter med
 alder = int(input('Hvilket år er du født? ') )
-Programmet skal så regne ut hvor gammel personen blir nå i løpet av år 2024 og skrive 
-svaret til skjerm med passende tekst."""
+Programmet skal så regne ut hvor gammel personen blir nå i løpet av år 2024 og skrive
+svaret til skjerm med passende tekst"""
 
+import datetime
+# Henter dagens år istden for å hardkode det til 2024
+year_now = datetime.datetime.now().year
+# spør bruker om hvilket år de er født
+alder = int(input("Hvilket år er du født? "))
 
-alder = int(input('Hvilket år er du født? '))
-årstall=2024
-
-print(f"Du blir {årstall-alder} år i løpet av 2024")
+print(f"Du blir i løpet av {year_now} {year_now - alder} år gammel.")
