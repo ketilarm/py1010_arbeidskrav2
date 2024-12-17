1fc637d017717c4c6317865716779a5352fdeeef
Ketil <102487845+ketilarm@users.noreply.github.com>
2024-12-17 13:18:03 +0100

    Update oppgave3.py

--- oppgave3.py
+++ oppgave3.py
@@ -1,7 +1,15 @@
-"""Oppg 3) Lag et program med en funksjon som regner om fra grader til radianer. 
-Programmet skal starte med: 
-import numpy as np 
+"""Oppg 3) Lag et program med en funksjon som regner om fra grader til radianer.
+Programmet skal starte med:
+import numpy as np
 v_grad = float(input('Skriv inn gradtallet:' ))
+
 Radiantallet til vinkelen regnes så ut ved følgende formel: v_rad = v_grad*np.pi/180
-Resultatet v_rad skrives til skjerm med passende tekst og verdi. 
+Resultatet v_rad skrives til skjerm med passende tekst og verdi.
 Merk: np.pi er en ferdiglaget funksjon som gir verdien 3.1415...."""
+
+
+import numpy as np
+v_grad = float(input("Skriv inn gradtallet:"))
+v_rad = v_grad * np.pi / 180
+
+print(f"Radiantallet til {v_grad:.0f}° er  {v_rad:.2f} radianer.")
